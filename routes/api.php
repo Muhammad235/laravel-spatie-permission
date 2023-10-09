f3182200f8a84695eb11fc6701561ef1c756b697
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-10-09 22:49:09 +0100

    completed api cofiguration

--- routes/api.php
+++ routes/api.php
@@ -16,23 +16,36 @@ use App\Http\Controllers\Auth\AdminController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
-
-
-Route::post('/login', [AdminController::class, 'login']);
-
+/**
+ *  The code below is how a typical route should be when using spatie and sanctum for authenciation
+ *
+ * **/
 
 
 //only admin and system admin can access this route
-Route::prefix('admin')->middleware(['auth:sanctum', 'role:super-admin|system-admin'])->group(function () {
 
-        Route::get('/users', function() {
-            return response()->json([
+// Route::prefix('admin')->middleware(['auth:sanctum', 'role:super-admin|system-admin'])->group(function () {
 
-                "users" => User::with('roles')->get()
-            ]);
-        });
+//         Route::get('/users', function() {
+//             return response()->json([
+
+//                 "users" => User::with('roles')->get()
+//             ]);
+//         });
+// });
+
+
+// Route::post('/login', [AdminController::class, 'login']);
+
+
+
+$api = app('Dingo\Api\Routing\Router');
+
+
+$api->version('v1', function ($api) {
+
+    $api->get('hello', function(){
+        return "config";
+    });
 });
