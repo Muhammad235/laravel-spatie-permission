b3faf4b1301f443ae3e42d4de9e5d0bfc1824ddd
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-10-16 15:37:28 +0100

    [ADD] store resource

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UserProfileSeeder extends Seeder
+class StoreSeeder extends Seeder
 {
     /**
      * Run the database seeds.
